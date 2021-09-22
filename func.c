f8aa92585d649c292c0ac82051eeda2ecb7d761f
Adebisi Yusuf <65837990+codewithade@users.noreply.github.com>
2021-09-22 18:18:55 +0100

    Updated the file

--- func.c
+++ func.c
@@ -10,8 +10,8 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
-#define MAXLINE 100
-
+// Uses the getsockname() function to get the
+// socket address family from the socket file descriptor (sockfd)
 int sockfd_to_family(int sockfd) {
 	struct sockaddr_storage ss;
 	socklen_t len;
@@ -22,31 +22,8 @@ int sockfd_to_family(int sockfd) {
 	return (ss.ss_family);
 }
 
-// reads a max of 19 chars from command line
-char *get_line(){
-	int numbytes = 80;
-	char line[numbytes];
-	return fgets(line, numbytes, stdin);
-}
 
-int get_char(char *buf, int size) {
-	int i = 0;
-	char c;
-	char sentence[size];
-	while((i<size-1) && (c = getchar()) != '\n') {
-		sentence[i++] = c;
-	}
-	sentence[i] = '\0';
-	buf = sentence;
-	return 0;
-}
 
-int main(int argc, char *argv[]) {
-	char data[20];
-	fgets(data, 20, stdin);
-	printf("Data: %s", data);
-	fgets(data, 20, stdin);
-	printf("Data: %s", data);
 	
 	
 	
